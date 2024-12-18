ddad734a420536be75edd9726c365fd23a5e6a0d
Julia <6527677+madewithlinux@users.noreply.github.com>
2024-12-18 15:26:55 -0700

    reformat code

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,36 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/timer.h"
 #include "hardware/clocks.h"
+#include "hardware/timer.h"
+#include "pico/stdlib.h"
 
 #include "bsp/board.h"
 #include "tusb.h"
 #include "usb_descriptors.h"
 
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+int64_t alarm_callback(alarm_id_t id, void* user_data) {
+  // Put your timeout handler code in here
+  return 0;
 }
 
+int main() {
+  board_init();
+  tud_init(BOARD_TUD_RHPORT);
+  stdio_init_all();
 
+  // Timer example code - This example fires off the callback after 2000ms
+  add_alarm_in_ms(2000, alarm_callback, NULL, false);
+  // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
 
+  printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
+  printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
+  // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
 
-int main()
-{
-    board_init();
-    tud_init(BOARD_TUD_RHPORT);
-    stdio_init_all();
-
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
-
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
-
-    while (true) {
-        tud_task();  // tinyusb device task
-        // printf("Hello, world!\n");
-        // sleep_ms(1000);
-    }
+  while (true) {
+    tud_task();  // tinyusb device task
+                 // printf("Hello, world!\n");
+                 // sleep_ms(1000);
+  }
 }
-
 
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
